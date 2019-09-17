f3529589605dbbdfa476258d279f97878f129958
Billy Bright <billybright1992@gmail.com>
2019-09-16 18:14:32 -0700

    surver running, database set up, burger controller set up, as well as seeds for database set

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Beef Burger', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Lamb Burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Lion Burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Elk Burger', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Fish Burger', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Tofu Burger', true);
